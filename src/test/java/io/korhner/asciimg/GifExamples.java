0022a02af139f94a30317de63a3ca0c1a908cf0a
hoijui <hoijui.quaero@gmail.com>
2018-03-18 00:31:33 +0100

    cleanup test sources

--- src/test/java/io/korhner/asciimg/GifExamples.java
+++ src/test/java/io/korhner/asciimg/GifExamples.java
@@ -3,23 +3,12 @@ package io.korhner.asciimg;
 import io.korhner.asciimg.image.AsciiImgCache;
 import io.korhner.asciimg.image.character_fit_strategy.BestCharacterFitStrategy;
 import io.korhner.asciimg.image.character_fit_strategy.StructuralSimilarityFitStrategy;
-import io.korhner.asciimg.image.converter.AsciiToImageConverter;
 import io.korhner.asciimg.image.converter.GifToAsciiConvert;
-import io.korhner.asciimg.utils.AnimatedGifEncoder;
-import io.korhner.asciimg.utils.GifDecoder;
-
 import java.awt.Font;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.imageio.ImageIO;
 
 public class GifExamples {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 
 		// initialize caches
 		AsciiImgCache smallFontCache = AsciiImgCache.create(new Font("Courier",Font.BOLD, 6));
@@ -28,7 +17,7 @@ public class GifExamples {
 		
 		String srcFilePath = "examples/test.gif";
 		String disFilePath = "examples/test-ascii.gif";
-		int delay = 100;//ms
+		int delay = 100; // ms
 		
 		GifToAsciiConvert asciiConvert = new GifToAsciiConvert(smallFontCache, ssimStrategy);
 		
