77b0744aabfb7ad4f0507c19d5dbee084931a8cb
hoijui <hoijui.quaero@gmail.com>
2018-09-09 10:36:34 +0200

    simplify & speedup image export (removing double buffering)

--- src/main/java/io/korhner/asciimg/image/exporter/ImageAsciiExporter.java
+++ src/main/java/io/korhner/asciimg/image/exporter/ImageAsciiExporter.java
@@ -3,7 +3,6 @@ package io.korhner.asciimg.image.exporter;
 import io.korhner.asciimg.image.AsciiImgCache;
 import io.korhner.asciimg.image.matrix.ImageMatrix;
 import io.korhner.asciimg.image.matrix.TiledImageMatrix;
-import io.korhner.asciimg.utils.ArrayUtils;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.Map.Entry;
@@ -14,9 +13,6 @@ import java.util.Map.Entry;
 public class ImageAsciiExporter implements AsciiExporter<BufferedImage> {
 
 	private AsciiImgCache characterCache;
-	private int imageWidth;
-	private int imageHeight;
-	private int[] outputImagePixels;
 	private BufferedImage output;
 
 	public ImageAsciiExporter() {}
@@ -42,23 +38,16 @@ public class ImageAsciiExporter implements AsciiExporter<BufferedImage> {
 		for (int cpx = 0; cpx < characterEntry.getValue().getDimensions().getWidth(); cpx++) {
 			for (int cpy = 0; cpy < characterEntry.getValue().getDimensions().getHeight(); cpy++) {
 				final int component = (int) characterEntry.getValue().getValue(cpx, cpy);
-				outputImagePixels[ArrayUtils.convert2DTo1D(
+				getOutput().setRGB(
 						startCoordinateX + cpx,
 						startCoordinateY + cpy,
-						imageWidth)]
-						= new Color(component, component, component).getRGB();
+						new Color(component, component, component).getRGB());
 			}
 		}
 	}
 
-	/**
-	 * Write pixels to output image.
-	 */
 	@Override
-	public void imageEnd() {
-		this.getOutput().setRGB(0, 0, imageWidth, imageHeight, outputImagePixels, 0, imageWidth);
-
-	}
+	public void imageEnd() {}
 
 	/**
 	 * Create an empty buffered image.
@@ -66,10 +55,9 @@ public class ImageAsciiExporter implements AsciiExporter<BufferedImage> {
 	@Override
 	public void init(final TiledImageMatrix<?> source) {
 
-		this.imageWidth = source.getImageDimensions().getWidth();
-		this.imageHeight = source.getImageDimensions().getHeight();
-		this.outputImagePixels = new int[imageWidth * imageHeight];
-		this.output = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
+		final int imageWidth = source.getImageDimensions().getWidth();
+		final int imageHeight = source.getImageDimensions().getHeight();
+		output = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_ARGB);
 	}
 
 	@Override
